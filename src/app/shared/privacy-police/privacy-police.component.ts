6b9fc6131222106b1cad034a0bce004632893ef7
Regige <144334703+Regige@users.noreply.github.com>
2024-08-05 20:30:01 +0200

    Added privacy police

--- src/app/shared/privacy-police/privacy-police.component.ts
+++ src/app/shared/privacy-police/privacy-police.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-privacy-police',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './privacy-police.component.html',
   styleUrl: './privacy-police.component.scss'
 })
