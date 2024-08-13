6104788916ba1c17e876a62c0a0f7e49caddd9d0
Regige <144334703+Regige@users.noreply.github.com>
2024-08-13 17:18:27 +0200

    Added data service

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-main-page',
@@ -10,6 +11,39 @@ import { HeaderComponent } from '../shared/header/header.component';
 })
 export class MainPageComponent {
 
+  error = '';
 
+  constructor(public data: DataService) {}
+
+  async ngOnInit() {
+    try {
+        
+      this.data.videos = await this.data.loadVideos();
+        
+        // const rawVideos: any = await this.data.loadVideos();
+        
+        // this.data.videos = this.mapContacts(rawContacts);
+
+      console.log(this.data.videos);
+
+    } catch(e) {
+
+      console.log(e);
+      this.error = 'Fehler beim Laden!';
+    }
+
+  }
+
+
+  // mapContacts(rawContacts: any[]): Contact[] {
+  //     return rawContacts.map(contact => ({
+  //       id: contact.id,
+  //       title: contact.title,
+  //       email: contact.email,
+  //       phone: contact.phone,
+  //       hex_color: contact.hex_color,
+  //       logogram: contact.logogram
+  //     } as Contact));
+  //   }
 
 }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  // thumbnails = []; //: thumbnail[]
+  videos:any = [];
+  // selectedVideo = [];
+  // urlThumbnails = environment.baseUrl + '/videos/thumbnails/';
+  urlVideo = environment.baseUrl + '/videos/all/';
+  
+  constructor(private http: HttpClient) { }
+
+
+  // loadThumbnails() {
+
+  //   return lastValueFrom(this.http.get(this.urlThumbnails));
+  // }
+
+
+  loadVideos() {
+    return lastValueFrom(this.http.get(this.urlVideo));
+  }
+
+
+}
